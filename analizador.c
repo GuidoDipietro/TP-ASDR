6ac3c4978c46e4a2cab238a8b3e6930793acbe9a
GuidoDipietro <dipietroguido@gmail.com>
2020-10-23 15:03:45 -0300

    main

--- analizador.c
+++ analizador.c
@@ -18,7 +18,48 @@ int tabla[NUMESTADOS][NUMCOLS] =
 	{14,14,14,14,14,14,14,14,14,14,14,14,14}	// 14 err
 };
 
-int main(){
+void Objetivo (void){
+	programa();
+	match (fdt);
+	return;
+}
 
-	return 0;
+void ListaSentencias (void){
+	//<listaSentencias> -> <sentencia> {<sentencia>}
+	Sentencia()// la primera que detecta
+	while(1){//ciclo infinito
+	switch (ProximoToken()){
+		case ID:
+		case LEER:
+		case ESCRIBIR: //detecto un token correcto
+		sentencia(); //procesa la otra sentencia
+		break;
+		default: return;
+		}//del análisis de caso
+	}// de la repeticion
+}// del proceso
+
+
+
+/***************************Programa Principal************************/
+int main(int argc, char * argv[]){
+	TOKEN tok; char nomArchi[TAMNOM]; int l;
+
+	if ( argc == 1 ) { printf("......\n"); return -1; } // no puso nombre de archivo fuente
+	if ( argc != 2 ) { printf("......"); return -1; } //los argumentos deben ser 2
+
+	strcpy(nomArchi, argv[1]); l = strlen(nomArchi);
+	if ( l > TAMNOM ) {
+		printf("Nombre incorrecto del Archivo Fuente\n");
+		return -1;
+	}
+
+	if ( nomArchi[l-1] != 'm' || nomArchi[l-2] != '.' ) {
+		printf("Nombre incorrecto del Archivo Fuente\n");
+		return -1;
+	}
+
+	if ( (in = fopen(nomArchi, "r") ) == NULL ) {
+		printf("No se pudo abrir archivo fuente\n"); return -1; //no pudo abrir archivo
+	}
 }
